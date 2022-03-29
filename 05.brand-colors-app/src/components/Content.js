95aabd53849be54fd685c43f11a5889185382407
Ahmet Osman Sezgin <ahmet.osmn@outlook.com>
2022-03-29 19:48:46 +0300

    secilen renkleri indirme işlemi eklendi

--- 05.brand-colors-app/src/components/Content.js
+++ 05.brand-colors-app/src/components/Content.js
@@ -1,16 +1,18 @@
+import LazyLoad from "react-lazyload";
+import { useContext } from "react";
+import MainContext from "../MainContext";
 import Search from "./Search";
 import Brand from "./Brand";
-import MainContext from "../MainContext";
-import { useContext } from "react";
-import LazyLoad from "react-lazyload";
+import Download from "./Download";
 
 function Content() {
-  const { brands } = useContext(MainContext);
+  const { brands, selectedBrands } = useContext(MainContext);
 
   return (
     <main className="content">
       <header className="header">
         <Search />
+        {selectedBrands.length !== 0 && <Download/>}
       </header>
       <section className="brands">
         {brands.map((brand) => (
---
+++ 05.brand-colors-app/src/components/Download.js
@@ -0,0 +1,87 @@
+import { useContext, useEffect, useState } from "react";
+import MainContext from "../MainContext";
+import { GrLink, GrDownload, GrClose } from "react-icons/gr";
+
+function Download() {
+  const { selectedBrands, brands, setSelectedBrands } = useContext(MainContext);
+  const [downloadUrl, setDownloadUrl] = useState();
+  const [cssMethod, setCssMethod] = useState("css");
+
+  useEffect(() => {
+    if (selectedBrands.length > 0) {
+      let output = "";
+
+      switch (cssMethod) {
+        case "css":
+          output += ":root{\n";
+          selectedBrands.map((slug) => {
+            let brand = brands.find((brand) => brand.slug === slug);
+            brand.colors.map((color, key) => {
+              output += `--${slug}-${key}: #${color};\n`;
+            });
+          });
+          output += "}";
+          break;
+
+        case "scss":
+          selectedBrands.map((slug) => {
+            let brand = brands.find((brand) => brand.slug === slug);
+            brand.colors.map((color, key) => {
+              output += `\$${slug}-${key}: #${color};\n`;
+            });
+          });
+          break;
+
+        case "less":
+          selectedBrands.map((slug) => {
+            let brand = brands.find((brand) => brand.slug === slug);
+            brand.colors.map((color, key) => {
+              output += `@${slug}-${key}: #${color};\n`;
+            });
+          });
+          break;
+      }
+
+      const blob = new Blob([output]);
+      const url = URL.createObjectURL(blob);
+      setDownloadUrl(url);
+      return () => {
+        URL.revokeObjectURL(url);
+        setDownloadUrl("");
+      };
+    }
+  }, [selectedBrands, cssMethod]);
+
+  const getLink = () => {
+    prompt(
+      "Here's thu URL to share",
+      `httlp://localhost:3000/collection/${selectedBrands.join(",")}`
+    );
+  };
+
+  return (
+    <div className="download">
+      <div className="actions">
+        <a download={`brands.${cssMethod}`} href={downloadUrl}>
+          <GrDownload />
+        </a>
+        <select onChange={(e) => setCssMethod(e.target.value)}>
+          <option value="css">CSS</option>
+          <option value="scss">SCSS</option>
+          <option value="less">LESS</option>
+        </select>
+        <button onClick={getLink}>
+          <GrLink />
+        </button>
+      </div>
+      <div className="selected" onClick={() => setSelectedBrands([])}>
+        <GrClose />
+        {selectedBrands.length} brands collected
+      </div>
+    </div>
+  );
+}
+
+export default Download;
+
+// 1:00:00 dakaldım
