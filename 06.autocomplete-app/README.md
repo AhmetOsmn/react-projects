c683ba4f630ccb564bd81178a02bc9eafc5db658
Ahmet Osman Sezgin <ahmet.osmn@outlook.com>
2022-04-02 00:46:03 +0300

    ulke bilgileri ekranda gosterildi

--- 06.autocomplete-app/README.md
+++ 06.autocomplete-app/README.md
@@ -2,9 +2,13 @@
 
 Merhaba, bu projeyi PROTOTÜRK kanalındaki [şu eğitimi](https://www.youtube.com/watch?v=2yGhjADh20M) takip ederek geliştiriyoum.
 
+API erişimini [buradan](https://restcountries.com/#api-endpoints-v3-all) yapıyorum.
+
 # [Demo]()
 
 # Paketler
+
+        yarn add react-currency-format
 
 # Ekran Görüntüleri
 
--- 06.autocomplete-app/src/App.js
+++ 06.autocomplete-app/src/App.js
@@ -1,20 +1,20 @@
 import { useEffect, useRef, useState } from "react";
+import CurrencyFormat from "react-currency-format";
 
 function App() {
   const [search, setSearch] = useState("");
   const [result, setResult] = useState(false);
   const [data, setData] = useState([]);
   const searchRef = useRef();
+  var CurrencyFormat = require("react-currency-format");
 
   const isTyping = search.replace(/\s+/, "").length > 0;
 
   useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then(response => response.json())
-      .then(response => setData(response))
-  }, [])
-
-  console.log(data)
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((response) => setData(response));
+  }, []);
 
   useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside);
@@ -41,32 +41,47 @@ function App() {
   }, [search]);
 
   return (
-    <>
-      <div className="search" ref={searchRef}>
-        <input
-          className={isTyping ? "typing" : null}
-          value={search}
-          type="text"
-          placeholder="Bir ülke ara... (ingilizce)"
-          onChange={(e) => setSearch(e.target.value)}
-        />
-        {isTyping && (
-          <div className="search-result">
-            {result &&
-              result.map((item) => (
-                <div key={item.id} className="search-result-item">
-                  {item.name.common}
+    <div className="search" ref={searchRef}>
+      <input
+        className={isTyping ? "typing" : null}
+        value={search}
+        type="text"
+        placeholder="Bir ülke ara... (ingilizce)"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {isTyping && (
+        <div className="search-result">
+          {result &&
+            result.map((item) => (
+              <div key={item.id} className="search-result-item">
+                <img
+                  className="flag"
+                  src={item.flags.svg}
+                  alt={item.name.common}
+                />
+                <div className="info-div">
+                  <div className="info">
+                    <div className="title">{item.name.common}</div>
+                    <div className="population">
+                      <CurrencyFormat
+                        value={item.population}
+                        displayType={"text"}
+                        thousandSeparator={true}
+                      />
+                    </div>
+                  </div>
+                  <div className="capital">{item.capital}</div>
                 </div>
-              ))}
-            {!result && (
-              <div className="result-not-found">
-                "{search}" için sonuç bulunamadı.
               </div>
-            )}
-          </div>
-        )}
-      </div>
-    </>
+            ))}
+          {!result && (
+            <div className="result-not-found">
+              "{search}" için sonuç bulunamadı.
+            </div>
+          )}
+        </div>
+      )}
+    </div>
   );
 }
 
