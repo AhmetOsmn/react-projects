ba3abcf3c5b5ef866af80fac53388870727b9f9c
Ahmet Osman Sezgin <ahmet.osmn@outlook.com>
2022-04-04 21:23:54 +0300

    gereksiz dosyalar silindi

--- 07.sticky-note-app/src/App.js
+++ 07.sticky-note-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useRef, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const screen = useRef(null)
+
+  const handleKeyUp = (e) => {
+    if(e.key === 'c') {
+      console.log('c was pressed');
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={screen} tabIndex={0} onKeyUp={handleKeyUp} className='screen'>
+      test
     </div>
   );
 }
