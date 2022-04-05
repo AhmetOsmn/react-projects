9143939a003f4b72863969d8ee0358f612b55ded
Ahmet Osman Sezgin <ahmet.osmn@outlook.com>
2022-04-05 23:54:28 +0300

    context eklendi

--- 07.sticky-note-app/src/App.js
+++ 07.sticky-note-app/src/App.js
@@ -1,35 +1,40 @@
-import { useRef, useEffect, useState } from 'react';
-import './App.css';
+import { useRef, useEffect, useState } from "react";
+import MainContext from "./MainContext";
+import "./App.css";
 
 function App() {
-
-  const screen = useRef(null)
-  const [mode, setMode] = useState(false)
-  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const screen = useRef(null);
+  const [mode, setMode] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    screen.current.focus()
-  }, [])
+    screen.current.focus();
+  }, []);
 
   const handleKeyUp = (e) => {
-    if(e.key === 'c') {
-      setMode(!mode)
+    if (e.key === "c") {
+      setMode(!mode);
     }
-  }
+  };
 
   const handleMouseMove = (e) => {
-    setPosition({ x: e.pageX, y: e.pageY })
-  }
-
+    setPosition({ x: e.pageX, y: e.pageY });
+  };
 
   return (
-    <div ref={screen} tabIndex={0} onKeyUp={handleKeyUp} onMouseMove={handleMouseMove} className='screen'>
-      <img src='https://raw.githubusercontent.com/AhmetOsmn/frontend-examples/main/images/frontend-examples-3.png'/>
+    <MainContext.Provider>
+      <div
+        ref={screen}
+        tabIndex={0}
+        onKeyUp={handleKeyUp}
+        onMouseMove={handleMouseMove}
+        className="screen"
+      >
+        <img src="https://raw.githubusercontent.com/AhmetOsmn/frontend-examples/main/images/frontend-examples-3.png" />
 
-      <div style={{ position: 'fixed', left: position.x, top: position.y }}>Yorum yazmak için tıkla</div>
-
-      {mode && (<div>yorum modu aktif</div>)}
-    </div>
+        {mode && <div>yorum modu aktif</div>}
+      </div>
+    </MainContext.Provider>
   );
 }
 
---
+++ 07.sticky-note-app/src/components/LeaveCommentText.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LeaveCommentText() {
+  return (
+    <div>Yorum yazmak için tıkla</div>
+  )
+}
+
+export default LeaveCommentText
