3e4b7eb683d87aa24cb40109d8704ec7d2eed00a
Ahmet Osman Sezgin <ahmet.osmn@outlook.com>
2022-04-05 22:16:05 +0300

    mouse takip edilir hale getirildi

--- 07.sticky-note-app/src/App.js
+++ 07.sticky-note-app/src/App.js
@@ -1,20 +1,34 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
   const screen = useRef(null)
+  const [mode, setMode] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    screen.current.focus()
+  }, [])
 
   const handleKeyUp = (e) => {
     if(e.key === 'c') {
-      console.log('c was pressed');
+      setMode(!mode)
     }
+  }
+
+  const handleMouseMove = (e) => {
+    setPosition({ x: e.pageX, y: e.pageY })
   }
 
 
   return (
-    <div ref={screen} tabIndex={0} onKeyUp={handleKeyUp} className='screen'>
-      test
+    <div ref={screen} tabIndex={0} onKeyUp={handleKeyUp} onMouseMove={handleMouseMove} className='screen'>
+      <img src='https://raw.githubusercontent.com/AhmetOsmn/frontend-examples/main/images/frontend-examples-3.png'/>
+
+      <div style={{ position: 'fixed', left: position.x, top: position.y }}>Yorum yazmak için tıkla</div>
+
+      {mode && (<div>yorum modu aktif</div>)}
     </div>
   );
 }
